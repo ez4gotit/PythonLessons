e27d5ec48e9c4037b163580058604d8db4124fa8
Mikhail <98700194+ez4gotit@users.noreply.github.com>
2024-05-28 17:40:41 +0300

    Add:Lesson4

---
+++ Lesson4/main.py
@@ -0,0 +1,86 @@
+import os
+import time
+from pynput import keyboard 
+def clear():
+    print('\033[2J\033[H')
+
+
+
+def clear(sleepTime):
+    
+    time.sleep(sleepTime)
+    print('\033[2J\033[H')
+
+
+def printTriangle(height, xOffset, yOffset, ch):
+    widthDown = 2 * (height - 1) + 1
+    printChar("\n",yOffset)
+    for i in range(height):
+        width = 2 * i + 1
+        printChar(" ", xOffset)
+        space = int((widthDown - width) / 2)
+        printChar(" ", space)
+        for k in range(width):
+            if k != 0 and k != width-1 and i != height-1:
+                printChar(ch, 1)
+            else:
+                printChar("#", 1)
+        printChar(" ", space)
+        printChar("\n", 1)
+
+def printChar(ch, i):
+    for j in range(i):
+        print(ch, end='')
+
+
+
+xPos = 0
+yPos = 0
+size = 1
+print("Use WASD to Move Use QE to grow")
+def Input(xPos, yPos, size):
+    
+    with keyboard.Events() as events:
+        event = events.get(1e6)
+        key = event.key.char
+    if key == 'w':
+        yPos-=1
+    elif key == 's':
+        yPos+=1
+    elif key == 'a':
+        xPos-=1
+    elif key == 'd':
+        xPos +=1
+
+    if key == 'q':
+        size-=1
+    elif key == 'e':
+        size+=1
+    if xPos < 0:
+        xPos = 0
+    if yPos < 0:
+        yPos = 0
+    if size < 1:
+        size = 1
+    return xPos, yPos, size
+def printSquare(xx,yy,xOffset,yOffset,character):
+    for _ in range(yOffset):
+        print()
+
+    def printEnd():
+        print(" "*xOffset+"#"*xx)
+    def printRow():
+        print(" "*xOffset+"#"+character*(xx-2)+"#")
+
+    printEnd()
+    for _ in range(yy-2):
+        printRow()
+    printEnd()
+while True:
+    xPos, yPos,  size = Input(xPos=xPos, yPos=yPos, size=size)
+    clear(0.1)
+    printTriangle(size, xPos, yPos, " ")
+    #printSquare(size,size, xPos,yPos+size, " ")
+
+
+    
